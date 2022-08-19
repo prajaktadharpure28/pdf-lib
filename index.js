c866196605b0fd8cdfd88f8e91f836ab1342e167
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-08-19 17:31:43 +0530

    Added image

--- index.js
+++ index.js
@@ -13,20 +13,39 @@ async function createPdf() {
 
     const page = doc.addPage()
 
-    const {width, height} = page.getSize()
+    //add image to the page
 
-    let fontsize = 80
+    // let img = await PDFDocument.createImage(fs.readFileSync("./download.jpg"))
+
+    let img = fs.readFileSync("./download.jpg")
+
+    img = await doc.embedJpg(img)
+
+    //scale the image
+
+    const {width,height} = img.scale(1);
+
+    img.scale(1)
+
+    page.drawImage(img, {
+        x:page.getWidth() / 2 - width / 2,
+        y: page.getHeight() /2 - height / 2
+    })
+
+    // const {width, height} = page.getSize()
+
+    // let fontsize = 80
     // doc.addPage()
 
     //add some text to the page
 
-    page.drawText("Hello World",{
-        x: 60,
-        y: height - 4 * fontsize,
-        size: fontsize,
-        font: timesRomanFont,
-        color: rgb(0,0,0)
-    })
+    // page.drawText("Hello World",{
+    //     x: 60,
+    //     y: height - 4 * fontsize,
+    //     size: fontsize,
+    //     font: timesRomanFont,
+    //     color: rgb(0,0,0)
+    // })
     //save the document to a file
 
     fs.writeFileSync('output.pdf', await doc.save())
